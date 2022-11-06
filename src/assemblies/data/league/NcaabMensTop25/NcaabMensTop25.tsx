51cef42000c94d4daabec72895f82aff0aee6839
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2022-11-06 00:44:55 -0700

    NcaabMensTop25.

--- src/assemblies/data/league/NcaabMensTop25/NcaabMensTop25.tsx
+++ src/assemblies/data/league/NcaabMensTop25/NcaabMensTop25.tsx
@@ -1,7 +1,13 @@
 import React, {FC, ReactElement} from 'react';
+import { Wrapper } from '../../../../components';
+import { viusage } from '../../../../util';
 
-export const NCAAB_MENS_TOP25_CLASSNAMES : string[] = [ ];
-export const NCAAB_MENS_TOP25_STYLE : React.CSSProperties = {
+export const NCAAB_MENS_TOP25_CONTAINER_CLASSNAMES : string[] = [ ];
+export const NCAAB_MENS_TOP25_CONTAINER_STYLE : React.CSSProperties = {
+};
+
+export const NCAAB_MENS_TOP25_INNER_CLASSNAMES : string[] = [ ];
+export const NCAAB_MENS_TOP25_INNER_STYLE : React.CSSProperties = {
 };
 
 export type NcaabMensTop25Props = {
@@ -11,15 +17,21 @@ export type NcaabMensTop25Props = {
     classNames ? : string[];
     overrideClasses ? : boolean;
     responsive ? : boolean;
+    viusage ? : viusage.primary.Viusagelike
 };
 
 export const NcaabMensTop25 : FC<NcaabMensTop25Props>  = (props) =>{
 
     return (
-        <div
-        className={[...!props.overrideClasses ? NCAAB_MENS_TOP25_CLASSNAMES : [], ...props.classNames||[]].join(" ")}
-        style={{...!props.overrideStyle ? NCAAB_MENS_TOP25_STYLE : {}, ...props.style}}>
+        <Wrapper
+        viusage={props.viusage||"wrap"}
+        classNames={[...!props.overrideClasses ? NCAAB_MENS_TOP25_CONTAINER_CLASSNAMES : [], ...props.classNames||[]]}
+        style={{...!props.overrideStyle ? NCAAB_MENS_TOP25_CONTAINER_STYLE : {}, ...props.style}}>
+            <div
+            className={[...!props.overrideClasses ? NCAAB_MENS_TOP25_INNER_CLASSNAMES : [], ...props.classNames||[]].join(" ")}
+            style={{...!props.overrideStyle ? NCAAB_MENS_TOP25_INNER_STYLE : {}, ...props.style}}>
 
-        </div>
+            </div>
+        </Wrapper>
     )
 };
