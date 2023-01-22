9289b6a5f324565ac704eb639787db72b0ebd64b
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-01-22 10:56:33 -0800

    removing unused imports.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BlogApp } from './demos/BlogDemoApp'
-import { NcaabDemoApp } from './demos/NcaabDemoApp';
 import './index.css'
 import './main.css'
 import './github.css'
